d8993dee2b16f2e1ceda2fba5a6c53ff9bf339d6
brauliomeyer <braulio_meijer@yahoo.com>
2015-06-16 09:33:52 +0200

    Concept_company
    Concept company project

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="img/favicon.ico">
+
+        <title>BDR</title>
+
+        <!-- This is our Google Arvo font -->
+        <link href='http://fonts.googleapis.com/css?family=Arvo' rel='stylesheet' type='text/css'>
+
+        <!-- This is our Awesome Font -->
+        <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.css">
+
+        <!-- Custom styles for this template -->
+        <link href="css/jumbotron.css" rel="stylesheet">   
+
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="js/ie-emulation-modes-warning.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body id="jumbotron">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand btn-lg" title="BDR" href="#BDR">BDR</a>        
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="color-nav btn-lg"><a href="#jumbotron">BDR</a></li>
+                        <li class="color-nav btn-lg"><a href="#who-We-Are">Who we are?</a></li>
+                        <li class="color-nav btn-lg"><a href="#about">Get Involved!</a></li>
+                        <li class="color-nav btn-lg"><a href="#contact">Contact Us now</a></li>
+                    </ul>        
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div  class="jumbotron">
+            <div class="container">
+                <h1>BDR</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu lorem vitae nisl dignissim rutrum. Etiam euismod nisi ipsum, quis tincidunt purus scelerisque gravida. Morbi varius ex sit amet ipsum efficitur porttitor. Pellentesque sit amet efficitur nisl, sit amet euismod neque. Fusce eu convallis orci, at laoreet mauris. Suspendisse ac elit tristique, tempus turpis at, pharetra nisl.</p>
+                <p><a class="btn btn-primary btn-lg btn-danger" title="button See more" href="#" role="button">See more &raquo;</a></p>
+                <i title="B.D.R." class="fa fa-flask fa-5x"></i>
+            </div>
+            <div id="who-We-Are"></div>
+        </div>
+
+        <div id="get-involved">
+            <div class="container homepage who">
+                <h2 class="involved">Who We Are</h2>
+                <i id="fa-briefcase" class="fa fa-briefcase fa-5x"></i>
+                <hr>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <a class="active" href="#Nullam" target="_blank">Nullam eu lorem</a> vitae nisl dignissim rutrum. Etiam euismod nisi ipsum, quis tincidunt purus scelerisque gravida.</p>                
+                <div id="about"></div>
+            </div>
+
+            <div class="container homepage">     
+                <h2 class="involved">About</h2>             
+                <hr>
+                <!-- 4 row of columns -->
+                <div class="row">
+                    <div class="col-md-4">                        
+                        <div>
+                            <h2 id="company" class="h2Color">Company</h2>
+                            <i class="fa fa-users fa-5x"></i>
+                        </div>
+                        <hr>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. <span class="emphasis">Donec sed odio dui.</span></p>
+                        <p><a class="btn btn-info" href="#Company" target="_blank" role="button">View details &raquo;</a></p>
+                    </div>
+                    <div class="col-md-4">  
+                        <div>
+                            <h2 id="projects" class="h2Color">Projects</h2>
+                            <i class="fa fa-user-md fa-5x"></i>
+                        </div>
+                        <hr>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. <span class="emphasis">Donec sed odio dui.</span></p>
+                        <p><a class="btn btn-info" href="#Projects" target="_blank" role="button">View details &raquo;</a></p>
+                    </div>  
+                    <div class="col-md-4"> 
+                        <div>
+                            <h2 id="contact" class="h2Color">Contact</h2>
+                            <i class="fa fa-pencil-square-o fa-5x"></i>
+                        </div>
+                        <hr>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. <span class="emphasis">Donec sed odio dui.</span></p>
+                        <p><a class="btn btn-info" href="#Contact" target="_blank" role="button">View details &raquo;</a></p>
+                    </div>              
+                </div>
+            </div> <!-- /container -->            
+        </div>     
+
+        <footer>
+            <div style="padding-top: 70px;" class="container">
+                <a href="#jumbotron">
+                    <i title="B.D.R." class="fa fa-train fa-5x"></i>
+                </a>
+            </div>                          
+        </footer>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="js/ie10-viewport-bug-workaround.js"></script>
+    </body>
+</html>
